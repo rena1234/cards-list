b5e8c0be52789b9cd9c934eb20367076723bd0e9
Renato Melo <renato.f.junior@accenture.com>
2021-06-21 03:43:51 -0300

    new font colors and icons

--- src/app/components/results/results.component.ts
+++ src/app/components/results/results.component.ts
@@ -12,8 +12,12 @@ export class ResultsComponent implements OnInit {
 
   constructor(private searchContextService: SearchContextService) { }
   products: Product[] = [];
+  contextUpdateCounter = 0;
 
   ngOnInit(): void {
-    this.searchContextService.filteredOptions.subscribe( filteredOptions => this.products = filteredOptions);
+    this.searchContextService.filteredOptions.subscribe( filteredOptions => {
+      this.contextUpdateCounter += 1;
+      return this.products = filteredOptions
+    });
   }
 }
